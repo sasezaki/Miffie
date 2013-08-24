e52a6b432a43d95be66381abc8ab18822670d72e
sasezaki <sasezaki@gmail.com>
2013-08-25 01:30:05 +0900

    introduce composer settings

--- debug.php
+++ debug.php
@@ -1,12 +1,4 @@
 <?php
-function Miffie_autoload($class)
-{
-    $class = str_replace(array('_', '\\'), '/', $class);
-    if (file_exists(__DIR__. '/src/' . $class . '.php')) {
-        return include __DIR__. '/src/' . $class . '.php';
-    }
-}
-spl_autoload_register("Miffie_autoload");
-//require_once __DIR__.'/src/Miffie/Autoload.php';
+include_once __DIR__ . '/vendor/autoload.php';
 
 Miffie\CLIRunner::run($argv);
